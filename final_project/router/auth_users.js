8de2004b2f05ebc581836140a309899280894469
kodonou narcisse <narcissekodonou@gmail.com>
2025-02-09 11:01:04 -0500

    2nd cmmi

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,8 +35,40 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  let filtered_reviews = books[isbn].reviews;
+  let filter_users = Object.values(filtered_reviews).filter((review)=>review.user === req.user);
+  if(filter_users.length > 0){
+    let filter_review = filter_users[0];
+    
+    let review = req.query.review;
+    if(review){
+        filter_review.comment = review;
+    }
+    filtered_reviews = Object.values(filtered_reviews).filter((review)=> review.user != req.user);
+
+    filtered_reviews.push(filter_review);
+
+    res.status(200).json({message:`Review update`})
+  }
+  else{
+    filtered_reviews.push({
+        'comment' : req.query.review,
+        'user' : req.user,
+    })
+    res.status(200).json({message:`Review add`})
+  }
 });
+
+// Add a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Write your code here
+    const isbn = req.params.isbn;
+  
+    books[isbn].reviews = Object.values(filtered_reviews).filter((review)=>review.user != req.user);
+    res.status(200).json({message:`Review for ${req.user} delete`})
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
